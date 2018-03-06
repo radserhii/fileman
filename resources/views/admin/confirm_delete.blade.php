014420e5899819072ba5539ba3691baf3716b96c
Serhii <radserhii@gmail.com>
2018-03-06 23:51:27 +0200

    Fixed cancel confirmation for delete user

--- resources/views/admin/confirm_delete.blade.php
+++ resources/views/admin/confirm_delete.blade.php
@@ -4,6 +4,6 @@
     <div class="container text-center">
         <h2 class="text-danger">Ви впевненні?</h2>
         <a href="{{route('delete_user',['user' => $user])}}" class="btn btn-danger">Так</a>php
-        <a href="{{route('delete_user',['user' => $user])}}" class="btn btn-primary">Ні</a>
+        <a href="{{route('dashboard_users'}}" class="btn btn-primary">Ні</a>
     </div>
 @endsection
